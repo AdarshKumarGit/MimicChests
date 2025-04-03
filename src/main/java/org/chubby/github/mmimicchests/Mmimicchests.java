8ec30cffff8e6df5152a1bdeba24cc0d13131974
GityBoyy <adarsh43122@gmail.com>
2025-04-03 11:21:38 +0530

    Initial commit

---
+++ src/main/java/org/chubby/github/mmimicchests/Mmimicchests.java
@@ -0,0 +1,38 @@
+package org.chubby.github.mmimicchests;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(Mmimicchests.MODID)
+public class Mmimicchests {
+
+    public static final String MODID = "mmimicchests";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+
+    public Mmimicchests() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+
+    }
+
+    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+
+        }
+    }
+}
